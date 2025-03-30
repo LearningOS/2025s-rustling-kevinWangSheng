e3982027df7d52d41d06ffa692e8f8a696d84193
kevin_wang <ltwoggbuty2mh@gmail.com>
2025-03-30 12:35:03 +0800

    update

--- exercises/algorithm/algorithm1.rs
+++ exercises/algorithm/algorithm1.rs
@@ -157,8 +157,7 @@ mod tests {
         list.add(2);
         list.add(3);
         println!("Linked List is {}", list);
-        assert_eq!(2, list.length);
-
+        assert_eq!(3, list.length);
     }
 
     #[test]
